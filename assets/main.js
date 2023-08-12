9ea1f99e1a5e65eed6369510af32c3370ea319bc
mahmoud design <mahmouddesign129@gmail.com>
2023-08-13 00:10:03 +0300

    Add Files

---
+++ assets/main.js
@@ -0,0 +1,37 @@
+
+
+
+//acces 
+let inputText = document.querySelector(".results .inputs input")
+let resultText = document.querySelector(".results .result input")
+let button = document.getElementById("send")
+
+button.onclick = function sumbmet(e){
+    e.preventDefault();
+    calc()
+}
+
+
+function display(num ){
+    inputText.value +=  num
+}
+
+function Clear(){
+    inputText.value  = "";
+    resultText.value =  "";
+}
+function Delete(){
+    inputText.value  = inputText.value.slice(0,-1)
+
+}
+
+function calc(){
+    try{
+        resultText.value =  eval(inputText.value)
+    }
+    catch(err){
+        resultText.value =  "error"
+    }
+}
+
+
